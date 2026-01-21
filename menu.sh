15545ef2f7bb11e77f8b50a5867f0bf31491e129
Ewen <linktriforce2611@gmail.com>
2026-01-21 21:27:42 +0100

    okay script sans indentations

--- menu.sh
+++ menu.sh
@@ -6,9 +6,51 @@ then
     exit 1
 fi
 #_____________________FONCTIONS
-
-
 # Fonction responsable de l'affichage du contenu des fichiers
+
+function supprimer-suggestion ()
+{
+
+	echo "Quel type de suggestion voulez vous effacer ?"
+
+	input=0
+	while [ $input -ne 1 -a $input -ne 2 -a $input -ne 3 ]
+	do
+		echo "Veuillez sélectionner un dossier"
+		echo "(1)ENTREES  -  (2)PLATS  -  (3)DESSERTS"
+		read input
+	done
+
+	case $input in
+		1)
+			chemin="ENTREES"
+			;;
+		2)
+			chemin="PLATS"
+			;;
+		*)
+			chemin="DESSERTS"
+			;;
+	esac
+
+    echo "Veuillez saisir une suggestion à supprimer :"
+    ls ./SUGGESTIONS/$chemin
+    read input
+	echo "Êtes-vous sûr ? [Y/n]"
+    read confirmer
+	if [ $confirmer -eq "Y" ]
+	then
+
+    	if rm -R ./SUGGESTIONS/$chemin/$input
+    	then
+        	echo "La suggestion a été effacée" 
+    	else
+        	echo "Echec de la suppression"
+    	fi
+    fi
+	return 0
+}
+
 affichage_debut() {
         echo "Voici le contenu du fichier $1 :"
         head -n $2 $1
@@ -40,7 +82,6 @@ prix ()
         echo "Prix : $prix €" >> $1
 }
 
-
 function compter-nb-fichs-dans-reps ()
 {
 
@@ -69,6 +110,31 @@ function compter-nb-fichs-dans-reps ()
 	echo "Dans le dossier $chemin, il y a $nbfichs recettes."
 	return 0
 }
+
+function supprimer-menu {
+	echo "Veuillez saisir le menu à supprimer ou 'q' pour annuler"
+	ls ./MENUS
+	read input
+	case $input in
+		"q")
+			return 0
+			;;
+		*)
+			echo "Êtes vous certain de vouloir supprimer ce menu ? [Y/n]"
+			read confirmer
+			if [ $confirmer -eq "Y" ]
+			then
+				if rm -R ./MENUS/$input 2> /dev/null
+				then
+					echo "Le Menu a été supprimé."
+				else
+					echo "Echec de la suppression."
+				fi
+			fi
+			;;
+	esac
+	return 0
+}
 #_____________________MAIN LOOP
 input=0
 while [ $input -ne 12 ]
@@ -90,41 +156,33 @@ do
 
         case $input in
             1)
-                    input=1
                     ;;
             2)
-                    input=2
+                    supprimer-suggestion
                     ;;
             3)
-                    input=3
                     ;;
             4)
-                    input=4
-		    affichage_debut()
+		    affichage_debut
                     ;;
             5)
-                    input=5
-		    affichage_fin()
+		    affichage_fin
                     ;;
             6)
-                    input=6
+                    compter-nb-fichs-dans-reps
                     ;;
             7)
-                    input=7
-                    note ()
+                    note
 		    ;;
             8)
-                    input=8
-		    prix ()
+		    prix
                     ;;
             9)
-                    input=9
                     ;;
             10)
-                    input=10
+                    supprimer-menu
                     ;;
             11)
-                    input=11
                     ;;
 
         esac
