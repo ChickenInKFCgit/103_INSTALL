637b209ed4b55356be0669a2e800d4e23804bb62
etyen <titimass1540@gmail.com>
2026-01-21 21:38:12 +0100

    sous-fonctions etienne

--- menu.sh
+++ menu.sh
@@ -7,7 +7,47 @@ then
 fi
 
 #_____________________FONCTIONS
-# Fonction responsable de l'affichage du contenu des fichiers
+
+function creerSuggestion ()
+{
+    : '
+    crée une suggestion d’entrée/plat/dessert. Le script demande un nom de fichier qui sera créé,
+    le script demande la saisie d’un texte qui sera écrit dans le fichier de la suggestion,
+    le script demande de quel type est la suggestion pour savoir où enregistrer le fichier (ENTREES/PLATS/DESSERTS).
+    On supposera que les recettes doivent avoir des noms de suggestion en entrée, plat et dessert différents
+    '
+
+    echo "Création d'une suggestion"
+
+    echo "Nom de la suggestion à créer :"
+    read nom
+
+    echo "Contenu de la recette :"
+    read contenu
+
+    type='x'
+    while [ $type != 'e' -a $type != 'p' -a $type != 'd' ]
+    do
+        echo "De quel type est la suggestion ?"
+        echo "Entrez e/p/d pour entrée, plat ou dessert"
+        read type
+    done
+
+    case $type in
+        e) dossier="ENTREES";;
+
+        p) dossier="PLATS";;
+
+        d) dossier="DESSERTS";;
+        
+        default) break;;
+    esac
+
+    echo $contenu > $dossier/$nom.txt
+
+    echo "Suggestion $nom créée dans $dossier"
+    echo "Fin de la création"
+}
 
 function supprimer-suggestion ()
 {
@@ -50,6 +90,51 @@ function supprimer-suggestion ()
         fi
     fi
     return 0
+}
+
+function consulterSuggestion ()
+{
+    : '
+    consulter une suggestion.
+    Le script demande de quel type est la suggestion
+    et affiche à l’écran le contenu d’une suggestion dont le nom est saisi ; 
+    l’affichage est précédé du nom du fichier
+    '
+    echo "Consultation d'une suggestion :"
+    echo
+
+
+    type='x'
+    while [ $type != 'e' -a $type != 'p' -a $type != 'd' ]
+    do
+        echo "Quel type de suggestion souhaitez-vous consulter ?"
+        echo "Entrez e/p/d pour entrée, plat ou dessert"
+        read type
+    done
+
+    case $type in
+        e) dossier="ENTREES";;
+
+        p) dossier="PLATS";;
+
+        d) dossier="DESSERTS";;
+        
+        default) break;;
+    esac
+
+    choix='x'
+    while [ ! -f $dossier/$choix ]
+    do
+        echo "Quelle suggestion souhaitez-vous consulter ?"
+        ls $dossier
+        echo
+        read choix
+    done
+
+    echo
+    echo "Affichage de $choix :"
+    echo
+    cat $dossier/$choix
 }
 
 affichage_debut() {
@@ -112,6 +197,55 @@ function compter-nb-fichs-dans-reps ()
     return 0
 }
 
+function composerMenu ()
+{
+    : '
+    composer un menu.
+    Le script crée un répertoire MENUS s’il n’existe pas
+    et crée un fichier dont le nom est saisi par l’utilisateur.
+    Le script demande à l’utilisateur comment Il souhaite 
+    composer son menu en lui proposant la liste des entrées, plats et desserts. 
+    Le script doit proposer à l’utilisateur de consulter les prix et les notes. 
+    L’utilisateur choisit pas à pas son menu, les noms des suggestions sont écrits dans le menu.
+    '
+
+    if [ ! -d MENUS ]
+    then
+        mkdir MENUS
+    fi
+
+    echo "Quel est le nom du menu que vous souhaitez créer ?"
+    read nom
+
+    echo "~ Composition du menu $nom ~"
+
+
+    # À ajouter :
+    # - vérification d'appartenance au dossier
+    # - choix de consultation des notes / prix 
+
+    echo "Choisissez une entrée :"
+    ls ENTREES
+    read choixEntree
+
+    echo "Choisissez un plat :"
+    ls PLATS
+    read choixPlat
+
+    echo "Choisissez un dessert :"
+    ls DESSERTS
+    read choixDessert
+
+    touch MENUS/$nom.txt
+    echo
+    echo "~ Menu $nom ~" > MENUS/$nom.txt
+    echo "Entrée : $choixEntree" >> MENUS/$nom.txt
+    echo "Plat : $choixPlat" >> MENUS/$nom.txt
+    echo "Dessert : $choixDessert" >> MENUS/$nom.txt
+    echo "Bon appétit !" >> MENUS/$nom.txt
+
+    cat MENUS/$nom.txt
+}
 function supprimer-menu {
     echo "Veuillez saisir le menu à supprimer ou 'q' pour annuler"
     ls ./MENUS
@@ -159,11 +293,13 @@ do
 
     case $input in
         1)
+            creerSuggestion
             ;;
         2)
             supprimer-suggestion
             ;;
         3)
+            consulterSuggestion
             ;;
         4)
             affichage_debut
@@ -181,6 +317,7 @@ do
             prix
             ;;
         9)
+            composerMenu
             ;;
         10)
             supprimer-menu
